2b492f70f4e61089483c5da32d61fc181305e9c3
Dmitry <dmitryd.prog@gmail.com>
2019-02-23 21:41:24 +0700

    Инкремент версии.

--- apps/serr_lint/mix.exs
+++ apps/serr_lint/mix.exs
@@ -4,7 +4,7 @@ defmodule SerrLint.MixProject do
   def project do
     [
       app: :serr_lint,
-      version: "0.1.0",
+      version: "0.2.0",
       build_path: "../../_build",
       config_path: "../../config/config.exs",
       deps_path: "../../deps",
